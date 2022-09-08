9beab5b17127d4bcf6762c6bf844543972113ea0
maranathaadesanya <adesanyamaranatha@yahoo.com>
2022-09-08 15:47:25 +0100

    Corrections on printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return:Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammer, but the outcome is a peice of art,\n");
 return (0);
 }
