2d846cfea059eaf94c7edda0aaea18f024caed1d
maranathaadesanya <adesanyamaranatha@yahoo.com>
2022-09-09 06:57:20 -0700

    Print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (on success)
+ */
+
+int main(void)
+{
+int n;
+
+n = 48;
+
+while (n <= 57)
+
+{
+
+putchar (n);
+
+if (n != 57)
+
+{
+
+putchar(',');
+
+putchar(' ');
+
+}
+
+n++;
+
+}
+
+putchar ('\n');
+
+return (0);
+}
