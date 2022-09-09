324fbf793ce6c5678eaace1603662827772cb8bb
maranathaadesanya <adesanyamaranatha@yahoo.com>
2022-09-09 07:09:19 -0700

    Two digits pairs

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * main - Loop through variables a and b, printing digits.
+ *
+ * Return: print to stdout all possible different combinations of two digits
+ */
+
+int main(void)
+{
+int a = '0';
+int b;
+
+while (a <= '9')
+
+{
+
+b = a + 1;
+
+while (b <= '9')
+
+{
+
+putchar(a);
+
+putchar(b);
+
+if (a == '8' && b == '9')
+
+{
+
+putchar('\n');
+
+}
+
+else
+
+{
+
+putchar(',');
+
+putchar(' ');
+
+}
+
+b++;
+
+}
+
+a++;
+
+}
+
+return (0);
+
+}
