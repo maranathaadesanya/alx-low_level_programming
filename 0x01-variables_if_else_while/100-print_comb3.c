fa54238d29d0816b1239668a6d467eb945d9abc8
maranathaadesanya <adesanyamaranatha@yahoo.com>
2022-09-09 07:12:16 -0700

    Two digit pairs

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,45 +12,24 @@ int a = '0';
 int b;
 
 while (a <= '9')
-
 {
-
 b = a + 1;
-
 while (b <= '9')
-
 {
-
 putchar(a);
-
 putchar(b);
-
 if (a == '8' && b == '9')
-
 {
-
 putchar('\n');
-
 }
-
 else
-
 {
-
 putchar(',');
-
 putchar(' ');
-
 }
-
 b++;
-
 }
-
 a++;
-
 }
-
 return (0);
-
 }
