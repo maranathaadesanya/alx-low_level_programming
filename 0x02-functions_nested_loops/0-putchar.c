c02b60fa425322d0dfc411fe0385cca14d8e88dc
maranathaadesanya <adesanyamaranatha@yahoo.com>
2022-09-13 05:41:18 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Print "_putchar" followed by a new line.
+ *
+ * Return: 0 (on success)
+ */
+
+int main(void)
+{
+	char putchar[] = "_putchar";
+	int i = 0;
+
+	while (putchar[i] != '\0')
+	{
+		_putchar(putchar[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
