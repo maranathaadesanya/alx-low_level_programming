6285a93296336013f28601ac786581240599a795
maranathaadesanya <adesanyamaranatha@yahoo.com>
2022-09-19 14:17:48 -0700

    Pointers and arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer to an int
+ * and updates the value it points to to 98
+ * @n: pointer to the variable to update
+ *
+ * Return: 0 on success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* swap_int - swaps the values of two integers
+* @a: take an int
+* @b: take an int
+*
+* Return: 0 on success
+*/
+
+void swap_int(int *a, int *b)
+{
+	int i = *a;
+
+	*a = *b;
+	*b = i;
+}
