f5faacdcaf39d662afd838ea654588b6bf4dc270
maranathaadesanya <adesanyamaranatha@yahoo.com>
2022-09-22 13:23:56 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,27 +11,34 @@
 
 void print_buffer(char *b, int size)
 {
-	int c, d;
+	int byte, index;
 
-	c = 0;
-	if (size <= 0)
+	for (byte = 0; byte < size; byte += 10)
 	{
-	printf("\n");
-	}
-	while (c < size)
+	printf("%08x: ", byte);
+	for (index = 0; index < 10; index++)
 	{
-	printf("%8.8x:", c);
-	d = 0;
-	while (d < 10)
-	{
-	printf("%02x", b[c + d]);
-	if ((d % 2 == 0 && d != 0) || (c + d > size - 1))
-	{
+	if ((index + byte) >= size)
+	printf("  ");
+	else
+	printf("%02x", *(b + index + byte));
+	if ((index % 2) != 0 && index != 0)
 	printf(" ");
 	}
-	d++;
+	for (index = 0; index < 10; index++)
+	{
+	if ((index + byte) >= size)
+	break;
+	else if (*(b + index + byte) >= 31 &&
+		*(b + index + byte) <= 126)
+	printf("%c", *(b + index + byte));
+	else
+	printf(".");
 	}
-	c += 10;
+	if (byte >= size)
+	continue;
 	printf("\n");
 	}
+	if (size <= 0)
+	printf("\n");
 }
