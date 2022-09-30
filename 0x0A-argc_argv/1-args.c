abc16ba6cee8f804d8d91d8d33aa2e96365bd6a9
maranathaadesanya <adesanyamaranatha@yahoo.com>
2022-09-30 15:20:05 -0700

    ARGUMENTS

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguenments passed to it
+ * followed by a new line.
+ * @argc: arguemnet count
+ * @argv: array of pointer to arguement strings
+ * Return: 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
